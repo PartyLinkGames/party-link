b34aca4d186f6b69ad79b155442c2bdc5280452f
Andressa <andressalsmenezes@gmail.com>
2022-11-03 22:44:51 -0300

    feat: finished-landingPage-style

---
+++ pages/LandingPages/index.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { BsFillMoonFill, BsInstagram, BsCircleFill } from "react-icons/bs";
+import { FaSun } from "react-icons/fa";
+
+import mainImg from "../../assets/boysPlaying.png";
+
+export default function LandingPage() {
+  return (
+    <>
+      <main className="main">
+        <div className="main_div">
+          <header className="col-center gap-4">
+            <p className="text-6xl logo">PartyLink</p>
+            <p className="text-white text-base">Find your game duo</p>
+          </header>
+
+          <Image alt="boys Playing" src={mainImg} className="sm:w-72"/>
+        </div>
+
+        <aside className="aside-dark">
+          <p className="hidden sm:flex sm:h-12 text-2xl logo mt-10">PartyLink</p>
+
+          <nav className="col-center w-4/5 gap-8 sm:gap-12 sm:mt-20">
+            <button className="btn_yellow">Login</button>
+            <button className="btn_yellow">Register</button>
+          </nav>
+
+          <div className="hidden sm:flex absolute bottom-4 gap-8">
+            <Link href={""} className="social">
+              <BsInstagram />
+            </Link>
+            <Link href={""} className="social">
+              <AiFillTwitterCircle />
+            </Link>
+          </div>
+        </aside>
+        <div className="mode">
+          <button className="ml-3 text-3xl text-amber-500">
+            <BsFillMoonFill />
+          </button>
+          <button className="text-3xl text-amber-500">
+            <FaSun />
+          </button>
+          <button>
+            <BsCircleFill className="text-white text-4xl absolute top-0.5 right-2" />
+          </button>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <nav className="nav_footer">
+          <Link href={""}>About Us</Link>
+          <Link href={""}>Contact</Link>
+          <Link href={""}>Developers</Link>
+        </nav>
+      </footer>
+    </>
+  );
+}
