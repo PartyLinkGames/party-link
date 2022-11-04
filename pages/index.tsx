bad26313043a75974deb5c2b11b6d90642ca18d3
Andressa <andressalsmenezes@gmail>
2022-11-03 22:44:51 -0300

    feat: finished-landingPage-style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import LandingPage from "./LandingPages";
 export default function Home() {
-  return (
-    <div className="App">
-      <h1>teste1</h1>
-    </div>
-  );
+  return <LandingPage></LandingPage>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        screens: {
+          sm: "426px",
+          md: "768px",
+          lg: "976px",
+          xl: "1440px",
+        },
+        colors: {
+          ocupacity: "#21252980",
+          primary: "#2C1471",
+          "primary-dark": "#23045A",
+        },
+        width: {
+          90: "90%",
+        },
+      },
+    },
   },
   plugins: [],
 };
