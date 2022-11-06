44ae5ca572dfea8b1856a26220cb8941eb47903b
Jhonatas <jhonatasoliveira021@gmail.com>
2022-11-06 12:50:44 -0300

    feat: initialize protected routes function

--- components/modalRegister/index.tsx
+++ components/modalRegister/index.tsx
@@ -68,7 +68,7 @@ export default function ModalRegister() {
         style={{
           transition: "0.5s ease-in-out",
           transform: modalRegisterIsOpen
-            ? "translateY(-20%)"
+            ? "translateY(-30%)"
             : "translateY(-200%)",
         }}
       >
---
+++ contexts/ContextUser.tsx
@@ -0,0 +1,4 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { app } from "../firebase/config";
+
+// export const
