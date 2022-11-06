f93bc1a9e2cd66c67beb6c6b8692c8baa01d3328
Jhonatas <jhonatasoliveira021@gmail.com>
2022-11-06 15:28:47 -0300

    feat: add form login request

--- hooks/useAuthentication.ts
+++ hooks/useAuthentication.ts
@@ -1,19 +1,21 @@
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
   updateProfile,
+  signOut,
 } from "firebase/auth";
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/router";
-
 import { useState, useEffect } from "react";
 import { iRegisterForm } from "../components/modalRegister";
 import { toast } from "react-toastify";
 import { app } from "../firebase/config";
-
+import { ILogin } from "./../components/modalLogin/interface";
 export const useAuthentication = () => {
   const auth = getAuth(app);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<null | string>(null);
   const router = useRouter();
   const createUser = async (data: iRegisterForm) => {
     setLoading(true);
@@ -29,7 +31,7 @@ export const useAuthentication = () => {
     } catch (error: unknown) {
       if (error instanceof Error) {
         if (error.message.includes("email-already-in-use")) {
-          toast.error("O email já está cadastrado", {
+          toast.error("Email already used", {
             theme: "dark",
             autoClose: 2000,
           });
@@ -39,8 +41,42 @@ export const useAuthentication = () => {
       setLoading(false);
     }
   };
+  const loginUser = async (data: ILogin) => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      console.log(response);
+      router.push("/dashboard");
+    } catch (error: unknown) {
+      console.log("caiu no catch");
+      if (error instanceof Error) {
+        console.log(error.message);
+        if (
+          error.message.includes("auth/user-not-found") ||
+          error.message.includes("auth/wrong-password")
+        ) {
+          toast.error("Invalid Email/Password", {
+            theme: "dark",
+            autoClose: 2000,
+          });
+        }
+      }
+    }
+  };
+  const logout = () => {
+    try {
+      signOut(auth);
+      router.push("/");
+    } catch (error) {}
+  };
   return {
     createUser,
     loading,
+    loginUser,
+    logout,
   };
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
 import { ModalProvider } from "../contexts/ContextModal";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ModalProvider>
+        <ToastContainer />
         <Component {...pageProps} />
       </ModalProvider>
     </>
