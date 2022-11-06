7ce077c03dc6a0ff1171fffa2820598e94a5b611
Jhonatas <jhonatasoliveira021@gmail.com>
2022-11-06 13:09:48 -0300

    feat: add context of user

--- contexts/ContextUser.tsx
+++ contexts/ContextUser.tsx
@@ -1,4 +1,21 @@
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/config";
+interface iUserContextProps {
+  children: React.ReactNode;
+}
 
-// export const
+export const UserContext = createContext({});
+
+export const UserProvider = ({ children }: iUserContextProps) => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<null | User>(null);
+  const auth = getAuth(app);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
+};
