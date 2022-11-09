441f9fcbe84fc92e4e3b62894ba4f59c79879766
Marcio Gabriel <marciogabrieloficial@hotmail.com>
2022-11-09 05:45:44 -0300

    feat : add id FireBase in button card hunt

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: "https://api.tibiadata.com/",
-  timeout: 1000,
+  timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
 });
