5a140585fb9f51aa825181f9c52142956f0e7c9f
Leonardo <leo.gindri@outlook.com>
2022-11-10 02:48:55 -0300

    feat:modalHuntingMarking-responsive

--- components/huntingHours/index.tsx
+++ components/huntingHours/index.tsx
@@ -31,11 +31,11 @@ export default function HuntingHours({
   };
 
   return (
-    <li className="p-2 flex flex-col   rounded-md gap-4 border   shadow-[0px_0px_6px_2px_#4a3086,0px_0px_15px_0px_#1e1b36]">
+    <li className="p-2 w-full  flex flex-col rounded-md gap-4 border   shadow-[0px_0px_6px_2px_#4a3086,0px_0px_15px_0px_#1e1b36]">
       <h3 className="leading-4 font-extrabold text-[14px] text-yellow-400 ">
         Horario de inicio da hunt: {hour}
       </h3>
-      <div className="flex justify-around">
+      <div className="gap-4 py-2  sm:py-0 sm:gap-0 w-full max-w-max overflow-x-auto px-2 flex sm:justify-around">
         {team?.map((char: any) => {
           return (
             <CardPlayerOnHunt
@@ -49,7 +49,7 @@ export default function HuntingHours({
         })}
         {team && team.length <= 3 ? (
           <button
-            className=" shadow-[0px_0px_6px_2px_#565f03,0px_0px_15px_0px_#02221a]  hover:shadow-[0px_0px_6px_2px_#9aaa05,0px_0px_15px_0px_#85bb08] w-[125px] max-h-[150px] bg-gradient-to-tr text-opacity-[0.5] hover:text-opacity-[100] text-yellow-300 font-extrabold from-[#0a316bc5] via-[#29096688] to-[#1a4e9196] rounded-md"
+            className=" shadow-[0px_0px_6px_2px_#565f03,0px_0px_15px_0px_#02221a] min-w-[125px]  hover:shadow-[0px_0px_6px_2px_#9aaa05,0px_0px_15px_0px_#85bb08] w-[125px] max-h-[150px] bg-gradient-to-tr text-opacity-[0.5] hover:text-opacity-[100] text-yellow-300 font-extrabold from-[#0a316bc5] via-[#29096688] to-[#1a4e9196] rounded-md"
             onClick={handleJoinInHunt}
           >
             join
